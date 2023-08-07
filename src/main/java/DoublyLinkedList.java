bd583f4e2ed70f7f4663deb1f126803e74e59786
BROOKXAT <108485252+BROOKXAT@users.noreply.github.com>
2023-08-07 09:27:43 +0100

    Insertion + deletion

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -12,7 +12,7 @@ public class DoublyLinkedList {
 
         @Override
         public String toString(){
-            return  this.previous +"<-- "+this.data+" -->"+this.next ;
+            return  Integer.toString(this.data);
         }
     }
     public Node head ;
@@ -25,14 +25,56 @@ public class DoublyLinkedList {
         this.head = new Node(data);
     }
     //
-
+    public Node getElementByIndex(int index){
+        if (this.head == null ){
+            System.out.println("empty list");
+            return  null ;
+        }
+        else {
+            Node currentnode = this.head ;
+            int counter = 0 ;
+            while (currentnode.next != null){
+                currentnode = currentnode.next ;
+                counter ++ ;
+                if (counter == index) return currentnode ;
+            }
+            System.out.println("index out of range");
+            return null ;
+        }
+    }
 
     // insertions
+
+    public void insertNodeByIndex(int index,int data){
+        if (head == null && index ==0) this.head = new DoublyLinkedList.Node( data) ;
+        int counter = 0 ;
+        DoublyLinkedList.Node currentnode = this.head ;
+        DoublyLinkedList.Node newnode = new DoublyLinkedList.Node(data);
+        while (currentnode.next != null){
+            if (counter == index-1){
+                newnode.next = currentnode.next ;
+                newnode.previous = currentnode ;
+                currentnode.next.previous = newnode;
+                currentnode.next = newnode ;
+                break;
+            }
+            counter++ ;
+            currentnode = currentnode.next ;
+
+        }
+        if (counter == index -1){
+
+            currentnode.next.previous = newnode ;
+            currentnode.next = newnode;
+        }
+        else System.out.println("index out of range");
+    }
     public void insertAtBeginning(int data){
         if (this.head == null) this.head = new Node(data);
         Node newnode = new Node(data) ;
         newnode.next = this.head ;
         this.head.previous = newnode ;
+        this.head = newnode ;
     }
 
     public void insertAtEnd(int data){
@@ -62,5 +104,59 @@ public class DoublyLinkedList {
         newnode.previous = oldPrevious ;
         newnode.next = existingnode ;
         existingnode.previous = newnode ;
+    }
+
+    // deletion
+
+    public void deleteFirstNode(){
+        if ( this.head == null) {
+            System.out.println("empty list");
+        }
+        this.head = this.head.next ;
+        this.head.previous = null ;
+        System.out.println("first element deleted with success");
+    }
+
+    public void deleteLastNode(){
+        if (this.head == null) {
+            System.out.println("empty linked list");
+        }
+        Node currentnode = this.head ;
+        while (currentnode.next != null){
+            currentnode = currentnode.next ;
+        }
+        currentnode.previous.next = null ;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        if (head == null) return "empty linked list" ;
+        String tostring = "" ;
+        DoublyLinkedList.Node currentnode = head ;
+        while (currentnode.next != null){
+            tostring += currentnode.data + "-->" ;
+            currentnode = currentnode.next ;
+        }
+        tostring += currentnode.data ;
+        return tostring ;
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dlinkedList = new DoublyLinkedList(54) ;
+        dlinkedList.insertAtBeginning(7);
+        dlinkedList.insertAtEnd(9);
+        Node existingnode = dlinkedList.getElementByIndex(1);
+        System.out.println(existingnode);
+        dlinkedList.insertAfterNode(existingnode,89);
+        dlinkedList.insertBeforNode(existingnode,770);
+        System.out.println(dlinkedList);
+        dlinkedList.insertNodeByIndex(2,2);
+        System.out.println(dlinkedList);
+        dlinkedList.deleteFirstNode();
+        dlinkedList.deleteLastNode();
+        System.out.println(dlinkedList);
     }
 }
