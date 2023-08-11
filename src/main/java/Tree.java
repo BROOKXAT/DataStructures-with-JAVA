0a4cfaa166197b1ff1b345fcec543c53d8ee5546
BROOKXAT <108485252+BROOKXAT@users.noreply.github.com>
2023-08-11 16:04:14 +0100

    find node + tostring

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -6,6 +6,7 @@ public class Tree {
         Node parent ;
         ArrayList<Node> sons = new ArrayList<Node>();
         public Node(int data) {
+            this.data = data ;
             this.parent = null ;
         }
 
@@ -17,7 +18,6 @@ public class Tree {
         public void deletSon(Node son){
             this.sons.remove(son) ;
         }
-
     }
 
     public Node root ;
@@ -27,15 +27,64 @@ public class Tree {
         this.root = new Node(data) ;
         this.height = 0 ;
     }
+    // find operations
+    public void findeDeepestNodeHelper(int data, int currentdepth, int deepestdeepth,Node currentnode,Node deepestnode) {
+        if (currentnode == null) return;
+        if (currentnode.data == data) {
+            if (deepestnode == null || currentdepth > deepestdeepth) {
+                deepestnode = currentnode;
+                deepestdeepth = currentdepth;
+            }
+        }
+        for (Node child : currentnode.sons) {
+            findeDeepestNodeHelper(data, currentdepth + 1, deepestdeepth, child, deepestnode);
+        }
+    }
+
+    public Node findDeepestNode(int data) {
+        Node currentnode = this.root;
+        int currentdepth = 0;
+        Node deepestnode = null ;
+        findeDeepestNodeHelper(data,0,0,currentnode,deepestnode);
+        return deepestnode;
+
+    }
+
     // add/delete nodes
     public void addNode(Node newnode, Node parentnode){
         newnode.parent = parentnode ;
         parentnode.addSon(newnode);
     }
+    // this function will delete the deepest node with the value given.(son of the node will be gone too as a result)
+    public void deleteNode(int data ){
 
-    @Override
-    public String toString() {
-        String treeTostring = "" ;
-        return treeTostring ;
+    }
+
+
+    private String generateTreeString(Node node, int depth) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("  ".repeat(Math.max(0, depth)));
+        stringBuilder.append(node.data).append("\n");
+
+        for (Node child : node.sons) {
+            stringBuilder.append(generateTreeString(child, depth + 1));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Tree test = new Tree(0) ;
+        Node node1= new Node(1) ;
+        Node node2= new Node(2) ;
+        test.addNode(node1,test.root);
+        test.addNode(node2,test.root);
+        Node node1son1 = new Node(11);
+        Node node1son2 = new Node(22);
+
+        node1.addSon(node1son1);
+        node1.addSon(node1son2);
+        String s = test.generateTreeString(test.root,2);
+        System.out.println(s);
     }
 }
